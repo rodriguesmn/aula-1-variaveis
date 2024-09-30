4354246cc5ef3223f445c8dcf5d84945d828173b
rodrigues <manuellar2007@gmail.com>
2024-09-30 20:06:18 -0300

    exercicos de js

--- exercicio5.js
+++ exercicio5.js
@@ -1,4 +1,12 @@
-let roupa
-let cabelo
-let unha 
+let pergRoupa = "qual a cor da sua roupa?"
+let pergCabelo = "seu cabelo é curto ou longo?"
+let pergUnha = "lixou sua unha hoje?"
+
+let respRoupa = "azul"
+let respCabelo = "longo"
+let respUnha = "sim"
+
+console.log(pergRoupa, "-", respRoupa)
+console.log (pergCabelo, "-", respCabelo)
+console.log (pergUnha,"-", respUnha)
 
